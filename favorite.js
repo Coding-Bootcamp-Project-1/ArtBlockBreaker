6c1a489dfba4ba55b676aa62a0eaf49aa0e3860d
flyingtadpole <archer.curry@northwestern.edu>
2023-08-31 07:23:42 -0500

    add base js file, begin structuring out page

---
+++ favorite.js
@@ -0,0 +1,5 @@
+var returnBtn = document.getElementById('returnBtnFP')
+var wordCloudBtn = document.getElementById('wordCloudBtnFP')
+var favDisplayEl = document.getElementById('dispFavContainer')
+var wordCloudDisplayEl = document.getElementById('wordCloudContainer')
+
