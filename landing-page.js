139d24bb0eb6abd6d3558474178b8fc3a8e2057f
trburke7 <trburke7@ymail.com>
2023-08-31 19:37:37 -0500

    Added variables and functions
    Co-authored-by: Travis <trburke7@ymail.com
    Co-authored-by: Archer Curry <archer.curry@northwestern.edu>

--- landing-page.js
+++ landing-page.js
@@ -1,9 +1,20 @@
 // create var for elements on page
 
-//write API function
+// searchfield
+var searchForm = document.getElementById('searchForm')
+// searchbutton
+var searchButton = document.getElementById('searchButton')
+// displaySect
+var displayArtCards = document.getElementById('')
+// favoritesPage
+var favPageLink = document.getElementById('')
 
+
+//write API function
 function getArt(){
-    var requestUrl = 'https://api.artic.edu/api/v1/artworks/search?q= ${PLACEHOLDER}'
+    var searchTerm = searchForm.value
+    
+    var requestUrl = `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}`
     
     fetch(requestUrl)
         .then(function(response){
@@ -11,10 +22,36 @@ function getArt(){
         })
         .then(function(data){
             console.log(data)
+            for(var i = 0; i < data.length; i++){
 
+                var displayCard = document.createElement('div')
+                var resultImage = document.createElement('img')
+                var discSect = document.createElement('p')
+                var favButton = document.createElement('button')
+
+                getImageSrc()
+                discSect.textContent = data.
+
+                displayCard.append(resultImage, resultImage, discSect, favButton)
+                displayArtCards.append(displayCard)
+
+
+
+            }
         })
 }
 
+function getImageSrc(){
+    var imgUrl = `https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg`
+
+
+}
+
+function favoritePage(){
+    window.location.assign("./favorite.html")
+
+}
 
 //Event listener for button click
-
+searchButton.addEventListener('click', getArt)
+favPageLink.addEventListener('click', favoritePage)
