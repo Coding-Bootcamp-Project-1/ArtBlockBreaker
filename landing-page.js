4368a6c937595b79217626b48f906ff3c4e9dd31
ChgDave <chgdave@gmail.com>
2023-08-31 22:56:30 -0500

    added hero Image and more comments for code and added display card styling and added more code comment

--- landing-page.js
+++ landing-page.js
@@ -15,9 +15,17 @@ var heroImage = document.getElementById("mainImage");
 
 //write API function for the getting the art images
 function getArt(e) {
+  // prevent default for form
   e.preventDefault();
+
+  // hide hero image
+  heroImage.classList.add("hidden");
+  // clear the display container
+  displayArtCards.innerHTML = "";
+
+  // construct ART Institute APR URL
   var searchTerm = searchInput.value;
-  var requestUrl = `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}`;
+  var requestUrl = `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}&size=10`;
 
   // fetch the Art Institute API for art ID
   fetch(requestUrl)
@@ -28,36 +36,68 @@ function getArt(e) {
       console.log(data);
       for (var i = 0; i < data.data.length; i++) {
         //var imageId = data.
-        //create HTML element for the display card
-        var displayCard = document.createElement("div");
-        var resultImage = document.createElement("img");
-        var discSect = document.createElement("p");
-        var favButton = document.createElement("button");
+        var altText = data.data[i].thumbnail.alt_text;
         // fetch the artwork image id to construct the image url
         fetch(data.data[i].api_link)
           .then(function (response) {
             return response.json();
           })
           .then(function (data) {
-            console.log(data);
-            var imageUrl = getImageSrc(data.data.image_id);
-            resultImage.src = imageUrl;
+            renderDispCard(data, altText);
           });
-
-        // discSect.textContent = data.
-        // append the element to the parent html element
-        displayCard.append(resultImage, discSect, favButton);
-        displayArtCards.append(displayCard);
       }
     });
 }
 
-// define the function to construct image URL
+// define function to construct image URL
 function getImageSrc(imageId) {
   var imgUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
   return imgUrl;
 }
 
+// define function to render images in display card
+function renderDispCard(data, altText) {
+  // hide the main hero page
+  //   heroImage.classList.add("hidden");
+  //   displayArtCards.innerHTML = "";
+  //create HTML element for the display card
+  var displayCard = document.createElement("div");
+  var cardImage = document.createElement("div");
+  var resultImage = document.createElement("img");
+  var favButton = document.createElement("button");
+
+  var cardDescription = document.createElement("div");
+  var titleYear = document.createElement("h3");
+  var artistName = document.createElement("p");
+  var artInfo = document.createElement("p");
+
+  var imageUrl = getImageSrc(data.data.image_id);
+
+  //add values or attributes to the HTML elements
+  resultImage.src = imageUrl;
+  favButton.textContent = "Add to Favorite";
+  titleYear.textContent = `${data.data.title}     ${data.data.date_end}`;
+  artistName.textContent = data.data.artist_title;
+  artInfo.textContent = altText;
+
+  //add CSS class to the HTML element;
+  displayCard.classList.add("displayCard");
+  cardImage.classList.add("cardImage");
+  resultImage.classList.add("searchResultImage");
+  favButton.classList.add("favoriteButton");
+  cardDescription.classList.add("description");
+  titleYear.classList.add("artName");
+  artistName.classList.add("artistName");
+  artInfo.classList.add("artInfo");
+
+  // append the element to the parent html element
+  cardImage.append(resultImage, favButton);
+  cardDescription.append(titleYear, artistName, artInfo);
+  displayCard.append(cardImage, cardDescription);
+  displayArtCards.append(displayCard);
+}
+
+// add function for favorite page
 function favoritePage() {
   window.location.assign("./favorite.html");
 }
